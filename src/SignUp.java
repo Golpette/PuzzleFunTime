c5a0198cd4b11b23f8584db6f3fec953ac5120bf
AndrewRigg <AndrewJRigg@gmail.com>
2016-09-29 00:18:14 +0100

    Save text file of user details

--- src/SignUp.java
+++ src/SignUp.java
@@ -37,9 +37,9 @@ public class SignUp extends JComponent implements ActionListener{
 	JLayeredPane layer;
 	JPanel panel;
 	JButton  signup, back;
-	JLabel intro, emailLabel, passwordLabel, passwordRetypeLabel;
+	JLabel intro, emailLabel, usernameLabel, passwordLabel, passwordRetypeLabel;
 	Font font, font2, font3;
-	JTextField email, password, passwordRetype;
+	JTextField email, username, password, passwordRetype;
 	ImageIcon image, image2;
 	Image newimg;
 	Image img;
@@ -55,7 +55,7 @@ public class SignUp extends JComponent implements ActionListener{
 		intro.setFont(font);
 		intro.setHorizontalAlignment(SwingConstants.CENTER);
 		intro.setOpaque(false);
-		intro.setBounds(0, 0, 500, 220);
+		intro.setBounds(0, 0, 500, 170);
 		panel = new JPanel(new GridBagLayout());
 		signup = new JButton("Sign Up");
 		signup.setFont(font3);
@@ -94,17 +94,23 @@ public class SignUp extends JComponent implements ActionListener{
 		
 		emailLabel = new JLabel("Email: ");
 		emailLabel.setFont(font2);
+		usernameLabel = new JLabel("Username: ");
+		usernameLabel.setFont(font2);
 		passwordLabel = new JLabel("Password: ");
 		passwordLabel.setFont(font2);
 		passwordRetypeLabel = new JLabel("Retype: ");
 		passwordRetypeLabel.setFont(font2);
 		email = new JTextField("");
 		email.setFont(font2);
+		username = new JTextField("");
+		username.setFont(font2);
 		password = new JTextField("");
-		pass1 = new JPasswordField("");
+		pass1 = new JPasswordField(20);
 		pass1.setFont(font2);
-		pass2 = new JPasswordField("");
+		pass1.setText("pass");
+		pass2 = new JPasswordField(20);
 		pass2.setFont(font2);
+		pass2.setText("pass");
 		passwordRetype = new JTextField("");
 		frame = new JFrame("Auto Puzzle Generator");
 		frame.setPreferredSize(new Dimension(500,400));
@@ -126,7 +132,7 @@ public class SignUp extends JComponent implements ActionListener{
 		c.gridy = 1;
 		c.gridwidth = 1;
 		c.insets = new Insets(0,10,0,0);
-		panel.add(emailLabel, c);
+		panel.add(usernameLabel, c);
 		
 		c.weightx = 5.0;
 		c.weighty = 0.1;
@@ -134,7 +140,7 @@ public class SignUp extends JComponent implements ActionListener{
 		c.gridy = 1;
 		c.gridwidth = 1;
 		c.insets = new Insets(0,0,0,0);
-		panel.add(email, c);
+		panel.add(username, c);
 		
 		c.weightx = 1.0;
 		c.weighty = 0.1;
@@ -142,7 +148,7 @@ public class SignUp extends JComponent implements ActionListener{
 		c.gridy = 2;
 		c.gridwidth = 1;
 		c.insets = new Insets(0,10,0,0);
-		panel.add(passwordLabel, c);
+		panel.add(emailLabel, c);
 		
 		c.weightx = 5.0;
 		c.weighty = 0.1;
@@ -150,7 +156,7 @@ public class SignUp extends JComponent implements ActionListener{
 		c.gridy = 2;
 		c.gridwidth = 1;
 		c.insets = new Insets(0,0,0,0);
-		panel.add(pass1, c);
+		panel.add(email, c);
 		
 		c.weightx = 1.0;
 		c.weighty = 0.1;
@@ -158,12 +164,28 @@ public class SignUp extends JComponent implements ActionListener{
 		c.gridy = 3;
 		c.gridwidth = 1;
 		c.insets = new Insets(0,10,0,0);
-		panel.add(passwordRetypeLabel, c);
+		panel.add(passwordLabel, c);
 		
 		c.weightx = 5.0;
 		c.weighty = 0.1;
 		c.gridx = 1;
 		c.gridy = 3;
+		c.gridwidth = 1;
+		c.insets = new Insets(0,0,0,0);
+		panel.add(pass1, c);
+		
+		c.weightx = 1.0;
+		c.weighty = 0.1;
+		c.gridx = 0;
+		c.gridy = 4;
+		c.gridwidth = 1;
+		c.insets = new Insets(0,10,0,0);
+		panel.add(passwordRetypeLabel, c);
+		
+		c.weightx = 5.0;
+		c.weighty = 0.1;
+		c.gridx = 1;
+		c.gridy = 4;
 		c.gridwidth = 1;
 		c.insets = new Insets(0,0,20,0);
 		panel.add(pass2, c);
@@ -171,7 +193,7 @@ public class SignUp extends JComponent implements ActionListener{
 		c.weightx = 1.0;
 		c.weighty = 0.0;
 		c.gridx = 0;
-		c.gridy = 4;
+		c.gridy = 5;
 		c.gridwidth = 2;
 		c.ipady = 10;
 		c.insets = new Insets(0,0,0,0);
@@ -232,25 +254,33 @@ public class SignUp extends JComponent implements ActionListener{
 		   out.write(text.getBytes());
 		} 
 	
+	@SuppressWarnings("null")
 	public void actionPerformed(ActionEvent e) {
 	
 		if(e.getSource() == signup){
-			char[] correctPass = new char[]{'p','a','s','s'};
-			char[] password = pass1.getPassword();
-			System.out.println(password);
+			char[] password1 = pass1.getPassword();
+			char[] password2 = pass2.getPassword();
+			String a = new String(password1);
+			//String b = new String(ID);
+			System.out.println(password1);
 		   if(!emailValidator.validate(email.getText().trim())) {
 		        System.out.print("Invalid Email ID");
 		        /*
 		           Action that you want to take. For ex. make email id field red
 		           or give message box saying invalid email id.
 		        */
-		   }else if(!Arrays.equals(password, correctPass)){
-			   System.out.println("Wrong password");
+		   }else if(!Arrays.equals(password1, password2)){
+			   System.out.println("Passwords do not match!");
+			   pass1.setText("");
+			   pass2.setText("");
+			   
 		   }else{
 			   System.out.println("Correct password");   
+			 
 			   String user = email.getText();
-				   try {
-					writeToFile("Users.txt", user);
+				   try{
+					   String str = "<user>\n\t<username>" + username.getText() + "</username>\n\t<email>"+user + "</email>\n\t<password>" + a + "</password>\n</user>\n\n";
+					    writeToFile("Users.txt", str);
 						puzzleLoader = new PuzzleLoader(email.getText());
 					} catch (IOException e1) {
 						e1.printStackTrace();
@@ -258,16 +288,8 @@ public class SignUp extends JComponent implements ActionListener{
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
-					frame.dispose();
+				   frame.dispose();
 			   }
-			   
-			   System.out.println("Correct password");
-			   try {
-					puzzleLoader = new PuzzleLoader(email.getText());
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-				frame.dispose();
 		   }
 		
 	
